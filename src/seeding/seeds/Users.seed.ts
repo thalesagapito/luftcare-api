924cc030ab4f866a768279bf7d6df6ee9da229b7
Thales Agapito <thales.agapito@leads2b.com>
2020-10-12 21:52:24 -0300

    add second admin to user seeder

--- src/seeding/seeds/Users.seed.ts
+++ src/seeding/seeds/Users.seed.ts
@@ -7,27 +7,42 @@ import UserRepository from '@/repositories/UserRepository';
 
 export default class CreateUsers implements Seeder {
   public async run(factory: Factory, connection: Connection): Promise<void> {
+    const defaultAdminEmail = 'admin@luftcare.com.br';
+    const secondAdminEmail = 'lucas@luftcare.com.br';
+    const defaultPatientEmail = 'paciente@luftcare.com.br';
+
     console.info('\n🙍‍♂️ Searching for existing users 🔍');
     const userRepository = connection.getCustomRepository(UserRepository);
-    const adminExists = await userRepository.findUserByEmail('admin@luftcare.com.br') !== undefined;
-    const patientExists = await userRepository.findUserByEmail('paciente@luftcare.com.br') !== undefined;
+    const adminExists = await userRepository.findUserByEmail(defaultAdminEmail) !== undefined;
+    const secondAdminExists = await userRepository.findUserByEmail(secondAdminEmail) !== undefined;
+    const patientExists = await userRepository.findUserByEmail(defaultPatientEmail) !== undefined;
 
     if (adminExists) console.info('🙍‍♂️ Default admin already exists 🚫');
+    if (secondAdminExists) console.info('🙍‍♂️ Second admin already exists 🚫');
     if (patientExists) console.info('🙍‍♂️ Default patient already exists 🚫');
 
     const shouldCreateAdmin = !adminExists;
+    const shouldCreateSecondAdmin = !secondAdminExists;
     const shouldCreatePatient = !patientExists;
 
     const admin = {
-      email: 'admin@luftcare.com.br',
+      email: defaultAdminEmail,
       name: 'Administrador',
       role: UserRole.ADMIN,
       phoneNumber: '(99) 99999-9999',
       passwordHash: await hashPassword('luftcaresecret'),
     };
 
+    const secondAdmin = {
+      email: secondAdminEmail,
+      name: 'Lucas Moreira',
+      role: UserRole.ADMIN,
+      phoneNumber: '(99) 99999-9999',
+      passwordHash: await hashPassword('luftcaresecret'),
+    };
+
     const patient = {
-      email: 'paciente@luftcare.com.br',
+      email: defaultPatientEmail,
       name: 'Paciente',
       role: UserRole.PATIENT,
       phoneNumber: '(99) 99999-9999',
@@ -36,6 +51,7 @@ export default class CreateUsers implements Seeder {
 
     // i know these could be concurrent, it's just easier this way
     if (shouldCreateAdmin) await userRepository.save(admin);
+    if (shouldCreateSecondAdmin) await userRepository.save(secondAdmin);
     if (shouldCreatePatient) await userRepository.save(patient);
   }
 }
